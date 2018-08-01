8d32e85266779b38ff843f5213882eb74ea0d306
Nicko229 <you@example.com>
2018-08-01 16:21:05 +1000

    completed Creating the JWT bug free

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds259711.mlab.com:59711/devconnector1`
+    mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds259711.mlab.com:59711/devconnector1`,
+    secretOrKey: 'secret'
 }
