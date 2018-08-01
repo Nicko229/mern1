75d1559f7bd1739f3799c1cba8a50de5974eba90
Nicko229 <you@example.com>
2018-08-01 13:02:13 +1000

    express section finished without bugs

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds259711.mlab.com:59711/devconnector1`
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
+const app = express();
+
+// db config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongodb
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
