0efc1793d56d89e75a21d9b72165a6e9e427c82b
Nicko229 <you@example.com>
2018-09-10 10:28:43 +1000

    postman not working but I think it is a problem with postman, not the code.

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,8 @@ const passport = require('passport');
 
 // Load input validation
 const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
+
 
 // Load user model
 const User = require('../../models/User');
@@ -63,6 +65,12 @@ router.post('/register', (req, res) => {
 // @desc Login user / returning JWT token
 // @access Public
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+    // check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -71,14 +79,15 @@ router.post('/login', (req, res) => {
         .then(user => {
             // check for user
             if (!user) {
-                return res.status(404).json({ email: 'User not found' })
+                errors.email = 'User not found';
+                return res.status(404).json(errors)
             }
             // check password
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
                         // user matched
-                        const payload = { id: user.id, name: user.name, avatar: user.avatar } // create JWT payload
+                        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // create JWT payload
                         // sign token
                         jwt.sign(payload,
                             keys.secretOrKey,
@@ -90,7 +99,8 @@ router.post('/login', (req, res) => {
                                 });
                             });
                     } else {
-                        return res.status(400).json({ password: 'Password incorrect' });
+                        errors.password = 'Password incorrect';
+                        return res.status(400).json(errors);
                     }
                 });
         });
